29fc9cb25ffa67f607b0fff687f5f6b88eb7e8bd
Clarisah Razendri Ingnasia <143934686+clarissaclea@users.noreply.github.com>
2025-05-31 22:00:00 +0700

    Update Database.java new

--- Database.java
+++ Database.java
@@ -1,13 +1,14 @@
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.util.HashMap;
 
-public class DatabaseConnection {
-    private static final String URL = "jdbc:mysql://localhost:3306/qeemla_db";
-    private static final String USER = "root";
-    private static final String PASSWORD = "";
+public class Database {
+    private static HashMap<String, String> users = new HashMap<>();
 
-    public static Connection getConnection() throws SQLException {
-        return DriverManager.getConnection(URL, USER, PASSWORD);
+    static {
+        users.put("admin", "123");
+        users.put("user", "pass");
+    }
+
+    public static boolean authenticate(String username, String password) {
+        return users.containsKey(username) && users.get(username).equals(password);
     }
 }
